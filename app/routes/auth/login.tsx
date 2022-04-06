5bdaffb874c7a5125e76f065aeb495a3960b60b7
Matthew Trontz <mtrontz@outlook.com>
2022-04-06 12:37:06 -0400

    Update and rename login.jsx to login.tsx

--- app/routes/auth/login.tsx
+++ app/routes/auth/login.tsx
@@ -1,7 +1,9 @@
 import * as yup from 'yup'
 import { Form, redirect, json } from 'remix'
 import { useActionData } from 'remix'
+import type { LoaderFunction } from "remix";
 import { db } from '~/utils/db.server'
+import { getUserId } from "~/utils/session.server";
 import { createUserSession, login, register } from '~/utils/session.server'
 import { getErrorMessage, buildValidationErrorFields } from '~/utils/misc'
 import { Button } from '~/components/button'
@@ -26,6 +28,64 @@ let schema = yup.object().shape({
     // .matches(/^[A-Za-z\d@$!%*#?&]{8,}$/, 'Must be eight characters long.')
     .required('Password is required.'),
 })
+
+export let loader: LoaderFunction = ({ request }) => {
+let userId = await getUserId(request);
+  let jokes = userId
+    ? await db.joke.findMany({
+        take: 100,
+        orderBy: { createdAt: "desc" },
+        include: { jokester: { select: { username: true } } },
+        where: { jokesterId: userId },
+      })
+    : [];
+
+ const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  if (!host) {
+    throw new Error("Could not determine domain URL.");
+  }
+  const protocol = host.includes("localhost") ? "http" : "https";
+  let domain = `${protocol}://${host}`;
+  const jokesUrl = `${domain}/jokes`;
+
+let rssString = `
+    <rss xmlns:blogChannel="${jokesUrl}" version="2.0">
+      <channel>
+        <title>Remix Jokes</title>
+        <link>${jokesUrl}</link>
+        <description>Some funny jokes</description>
+        <language>en-us</language>
+        <generator>Kody the Koala</generator>
+        <ttl>40</ttl>
+        ${jokes
+          .map((joke) =>
+            `
+            <item>
+              <title>${joke.name}</title>
+              <description>A funny joke called ${joke.name}</description>
+              <author>${joke.jokester.username}</author>
+              <pubDate>${joke.createdAt}</pubDate>
+              <link>${jokesUrl}/${joke.id}</link>
+              <guid>${jokesUrl}/${joke.id}</guid>
+            </item>
+          `.trim()
+          )
+          .join("\n")}
+      </channel>
+    </rss>
+  `.trim();
+
+  return new Response(rssString, {
+    headers: {
+      "Cache-Control": `public, max-age=${60 * 10}, s-maxage=${60 * 60 * 24}`,
+      "Content-Type": "application/xml",
+      "Content-Length": String(Buffer.byteLength(rssString)),
+    },
+  });
+};
+
+};
 
 function loginUser() {}
 
@@ -196,5 +256,12 @@ export default function Login() {
         </Form>
       </div>
     </div>
-  )
-}
+  );
+};
+
+
+
+
+
+
+
